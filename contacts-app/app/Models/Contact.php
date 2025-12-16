25eb6ae84e455231ddb0adb56e3aea166b0dd295
yasmineh-d <yassminehadad75@gmail.com>
2025-12-16 13:31:11 +0000

    tasks is done

--- contacts-app/app/Models/Contact.php
+++ contacts-app/app/Models/Contact.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Contact extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'phone_number',
+    ];
 }
+
--- contacts-app/routes/web.php
+++ contacts-app/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
